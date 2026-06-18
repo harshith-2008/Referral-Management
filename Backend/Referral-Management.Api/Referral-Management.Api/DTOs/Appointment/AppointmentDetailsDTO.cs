818f2f023e64f9fdc29718aefbb150f8d611b3a1
harshith-2008 <harshithhca@gmail.com>
2026-06-18 16:15:39 +0530

    Appointment endpoints

---
+++ Backend/Referral-Management.Api/Referral-Management.Api/DTOs/Appointment/AppointmentDetailsDTO.cs
@@ -0,0 +1,24 @@
+﻿public class AppointmentDetailsDTO
+{
+    public int AppointmentId { get; set; }
+
+    public int ReferralId { get; set; }
+
+    public DateOnly AppointmentDate { get; set; }
+
+    public TimeOnly AppointmentTime { get; set; }
+
+    public string AppointmentStatus { get; set; } = string.Empty;
+
+    public int PatientId { get; set; }
+
+    public string PatientName { get; set; } = string.Empty;
+
+    public string Mrn { get; set; } = string.Empty;
+
+    public int SpecialistId { get; set; }
+
+    public string SpecialistName { get; set; } = string.Empty;
+
+    public string ReferralReason { get; set; } = string.Empty;
+}
---
+++ Backend/Referral-Management.Api/Referral-Management.Api/DTOs/Appointment/UpdateAppointmentStatusDTO.cs
@@ -0,0 +1,6 @@
+﻿public class UpdateAppointmentStatusDTO
+{
+    public int AppointmentId { get; set; }
+
+    public int AppointmentStatusId { get; set; }
+}
